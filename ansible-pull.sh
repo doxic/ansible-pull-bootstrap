ffd00d3b30dbc57bac7dc5ef35d3de4bade04cb1
Your Name <you@example.com>
2019-01-20 14:23:51 +0100

    install python from package manager

--- ansible-pull.sh
+++ ansible-pull.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # Install git
-[ -x "$(command -v apt-get)" ] && apt-get update && apt-get install -y git
-[ -x "$(command -v yum)" ] && yum makecache fast && yum install -y git
+[ -x "$(command -v apt-get)" ] && apt-get update && apt-get install -y git python-pip
+[ -x "$(command -v yum)" ] && yum makecache fast && yum install -y git python-pip
 
 # Install pip and ansible
-easy_install pip
 pip install ansible
 
 # Run ansible-pull
